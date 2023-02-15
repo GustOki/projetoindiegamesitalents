5f6df4a95f0f2eb6e9e9e226c3f829e96550a962
Gustavo Oki <gustavooki.dev@gmail.com>
2023-02-15 17:29:56 -0300

    Finalizando pag web

---
+++ script/feedback.js
@@ -0,0 +1,8 @@
+//script feedback
+function like() {
+    document.getElementById("like").innerHTML = "<i class='bi bi-hand-thumbs-up-fill'></i>GOSTEI";
+}
+
+function dislike() {
+    document.getElementById("dislike").innerHTML = "<i class='bi bi-hand-thumbs-down-fill'></i>NÃO GOSTEI";
+}
